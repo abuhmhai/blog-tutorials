772c3a9dd9a7ea0e12a8a82050b8689eadc92937
Minh Hải <117704352+abuhmhai@users.noreply.github.com>
2024-08-17 13:53:23 +0700

    Update User.java

--- springboot-jwt-auth/src/main/java/com/tericcabrel/authapi/entities/User.java
+++ springboot-jwt-auth/src/main/java/com/tericcabrel/authapi/entities/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -38,7 +39,9 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + role.getName().toString());
+
+        return List.of(authority);
     }
 
     public String getPassword() {
@@ -131,5 +134,19 @@ public class User implements UserDetails {
                 ", updatedAt=" + updatedAt +
                 '}';
     }
+    @OneToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "role_id", referencedColumnName = "id", nullable = false)
+    private Role role;
+
+    public Role getRole() {
+        return role;
+    }
+
+    public User setRole(Role role) {
+        this.role = role;
+
+        return this;
+    }
+
 }
 
